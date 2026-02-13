2d42907a05ee8a7e4f60d9a66624b62f0eddbea8
Aditya Bharadwaj Kusumba <adityabharadwaj2005@gmail.com>
2026-02-13 15:21:53 +0530

    Update axios.js

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,4 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
-axios.defaults.baseURL = import.meta.env.VITE_API_URL || "https://tierhire.onrender.com";
-axios.defaults.withCredentials = true;
+const api = axios.create({
+  baseURL: "https://tierhire.onrender.com",
+  withCredentials: true,
+});
+
+export default api;
